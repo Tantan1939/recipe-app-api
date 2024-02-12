b4846b13a4948f95a513f2a3ebad0700ea6a25a6
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2024-02-12 18:04:59 +0800

    custom django admin GUI

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,45 @@
+"""
+Django admin customization.
+"""
 from django.contrib import admin
-from django.apps import apps
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
 
-for each_model in apps.get_app_config('core').models.values():
-    admin.site.register(each_model)
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
