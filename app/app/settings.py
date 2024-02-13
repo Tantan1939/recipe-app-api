71f44ddce96e414146a934f1af8e8b86d368b995
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2024-02-13 16:18:30 +0800

    API documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 PROJECT_APPS = [
@@ -135,3 +137,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}  # use the AutoSchema provided by drf_spectacular as default schema class for generating OpenAPI schemas.
